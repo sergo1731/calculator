0adba4a8ed00b7d39a89a75dd190da92cf22f91f
sergo1731 <160328823+sergo1731@users.noreply.github.com>
2026-09-14 00:14:20 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = 'pohudenie-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./index.html','./manifest.json'])));
+  self.skipWaiting();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
